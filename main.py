95e70e7ffff31694469948af472755aad6855b8d
jwhitlow45 <jwhitlow45@gmail.com>
2025-09-04 18:13:31 -0700

    feat:add OpenData511Service

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from pprint import pprint
+
+from services.OpenData511Service import OpenData511Agency, OpenData511Client
+
+OPEN_DATA_511_API_KEY = os.getenv("OPEN_DATA_511_API_KEY") or ""
+
+
 def main():
-    print("Hello from muni-transit-display!")
+    open_data_511_client = OpenData511Client(OPEN_DATA_511_API_KEY)
+    pprint(
+        open_data_511_client.get_transit_stop_monitoring(
+            OpenData511Agency.SFMTA, "13516"
+        ).json()
+    )  # 26th & De Haro, North
+    pprint(
+        open_data_511_client.get_transit_stop_monitoring(
+            OpenData511Agency.SFMTA, "13516"
+        ).json()
+    )  # 26th & De Haro, South
 
 
 if __name__ == "__main__":
---
+++ services/OpenData511Service.py
@@ -0,0 +1,43 @@
+from enum import StrEnum
+from typing import Any, Literal
+
+import httpx
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+class OpenData511Agency(StrEnum):
+    SFMTA = "SF"
+
+
+class OpenData511Client:
+    def __init__(self, api_token: str) -> None:
+        self._client = httpx.Client(
+            base_url="https://api.511.org", params={"api_key": api_token}
+        )
+
+    def _authenticated_request(
+        self,
+        method: str,
+        path: str,
+        *,
+        params: dict[str, Any] | None = None,
+        json_payload: dict[str, Any] | None = None,
+    ) -> httpx.Response:
+        response = self._client.request(method, path, params=params, json=json_payload)
+        response.raise_for_status()
+        return response
+
+    def get_transit_stop_monitoring(
+        self, agency: str, stopcode: str | int, format: Literal["json", "xml"] = "json"
+    ):
+        return self._authenticated_request(
+            "get",
+            "/transit/StopMonitoring",
+            params={
+                "agency": agency,
+                "stopcode": stopcode or None,
+                "format": format,
+            },
+        )
