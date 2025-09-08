ddd0de627fbe819aadd6b7c0193efdba65a8f3e4
jwhitlow45 <jwhitlow45@gmail.com>
2025-09-07 18:13:35 -0700

    feat:add method to convert TransitStopMonitortingResponse to DisplayInfoModel

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+from pprint import pprint
 
 from dotenv import load_dotenv
 
+from models.DisplayInfo import convert_transit_stop_monitoring_response_to_display_info_model
 from services.OpenData511 import OpenData511Client
 
 load_dotenv(".env")
@@ -21,10 +23,13 @@ def main():
             f"Environment variable '{open_data_stop_code_env_var_name}' must be set in .env file at project root"
         )
 
-    response = open_data_511_client.get_transit_stop_monitoring(OPEN_DATA_511_AGENCY_ID, open_data_stopcode_list[0])
-    arrival_time = response.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit[
-        0
-    ].MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
+    transit_stop_monitoring_response = open_data_511_client.get_transit_stop_monitoring(
+        OPEN_DATA_511_AGENCY_ID, open_data_stopcode_list[0]
+    )
+    display_info = convert_transit_stop_monitoring_response_to_display_info_model(transit_stop_monitoring_response)
+
+    pprint(display_info.model_dump())
+    print(type(display_info))
 
 
 if __name__ == "__main__":
