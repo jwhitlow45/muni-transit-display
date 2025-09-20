6c9536a97d3a627e54c2dff5068c2f0e7031ca17
jwhitlow45 <jwhitlow45@gmail.com>
2025-09-19 21:02:51 -0700

    refactor:move transit response to display info conversion function into OpenData511 data model

--- models/DisplayInfo.py
+++ models/DisplayInfo.py
@@ -3,7 +3,7 @@ from typing import List
 
 from pydantic import BaseModel
 
-from models.OpenData511 import OccupancyEnum, TransitStopMonitoringResponse
+from models.OpenData511 import OccupancyEnum
 
 
 class DisplayStopVisitModel(BaseModel):
@@ -22,36 +22,3 @@ class DisplayStopVisitModel(BaseModel):
 class DisplayInfoModel(BaseModel):
     response_timestamp: datetime
     display_stop_visit_list: List[DisplayStopVisitModel]
-
-
-def convert_transit_stop_monitoring_response_to_display_info_model(
-    transit_stop_monitoring_response: TransitStopMonitoringResponse,
-):
-    stop_monitoring = transit_stop_monitoring_response.ServiceDelivery.StopMonitoringDelivery
-    response_timestamp = stop_monitoring.ResponseTimestamp
-    display_stop_visit_list = [
-        DisplayStopVisitModel(
-            recorded_at=stop_visit.RecordedAtTime,
-            line_reference=stop_visit.MonitoredVehicleJourney.LineRef,
-            line_name=stop_visit.MonitoredVehicleJourney.PublishedLineName,
-            monitored=stop_visit.MonitoredVehicleJourney.Monitored,
-            in_congestion=stop_visit.MonitoredVehicleJourney.InCongestion,
-            occupancy=stop_visit.MonitoredVehicleJourney.Occupancy,
-            vehicle_at_stop=stop_visit.MonitoredVehicleJourney.MonitoredCall.VehicleAtStop
-            if stop_visit.MonitoredVehicleJourney.MonitoredCall
-            else None,
-            aimed_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
-            if stop_visit.MonitoredVehicleJourney.MonitoredCall
-            else None,
-            expected_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime
-            if stop_visit.MonitoredVehicleJourney.MonitoredCall
-            else None,
-            aimed_departure_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime
-            if stop_visit.MonitoredVehicleJourney.MonitoredCall
-            else None,
-        )
-        for stop_visit in stop_monitoring.MonitoredStopVisit
-        if stop_visit.MonitoredVehicleJourney  # optional from API so ensure existence
-    ]
-
-    return DisplayInfoModel(response_timestamp=response_timestamp, display_stop_visit_list=display_stop_visit_list)
--- models/OpenData511.py
+++ models/OpenData511.py
@@ -4,6 +4,8 @@ from typing import Annotated, List
 
 from pydantic import BaseModel, BeforeValidator, Field
 
+from models.DisplayInfo import DisplayInfoModel, DisplayStopVisitModel
+
 
 def boolean_parser(v: bool | str):
     if type(v) is bool:
@@ -82,3 +84,33 @@ class ServiceDeliveryModel(BaseModel):
 
 class TransitStopMonitoringResponse(BaseModel):
     ServiceDelivery: ServiceDeliveryModel
+
+    def convert_to_display_info(self):
+        stop_monitoring = self.ServiceDelivery.StopMonitoringDelivery
+        response_timestamp = stop_monitoring.ResponseTimestamp
+        display_stop_visit_list = [
+            DisplayStopVisitModel(
+                recorded_at=stop_visit.RecordedAtTime,
+                line_reference=stop_visit.MonitoredVehicleJourney.LineRef,
+                line_name=stop_visit.MonitoredVehicleJourney.PublishedLineName,
+                monitored=stop_visit.MonitoredVehicleJourney.Monitored,
+                in_congestion=stop_visit.MonitoredVehicleJourney.InCongestion,
+                occupancy=stop_visit.MonitoredVehicleJourney.Occupancy,
+                vehicle_at_stop=stop_visit.MonitoredVehicleJourney.MonitoredCall.VehicleAtStop
+                if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                else None,
+                aimed_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
+                if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                else None,
+                expected_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime
+                if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                else None,
+                aimed_departure_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime
+                if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                else None,
+            )
+            for stop_visit in stop_monitoring.MonitoredStopVisit
+            if stop_visit.MonitoredVehicleJourney  # optional from API so ensure existence
+        ]
+
+        return DisplayInfoModel(response_timestamp=response_timestamp, display_stop_visit_list=display_stop_visit_list)
