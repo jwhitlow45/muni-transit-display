a2fa24dc0416b17e47ffd1109b6369a7f763f0d8
jwhitlow45 <jwhitlow45@gmail.com>
2025-09-21 15:41:21 -0700

    feat:add main driver program

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
-import os
 import threading
+from collections import defaultdict
+from datetime import datetime, timezone
 from time import sleep
 
-from dotenv import load_dotenv
 from httpx import HTTPStatusError
 
-from models.DisplayInfo import DisplayInfoModel
-from modules.display_utils import Colors, get_status_led_colors, get_text_center_x_pos
+import modules.environment as env
+from models.DisplayInfo import DisplayInfoModel, StopVisitModel
+from modules.display_utils import (
+    Colors,
+    generate_display_line_row,
+    get_status_led_colors,
+    get_text_center_x_pos,
+    get_text_center_y_pos,
+)
 from modules.logger import logger
 from modules.rgbmatrix_configurer import get_rgb_matrix
 from modules.rgbmatrix_importer import get_rgb_matrix_imports
 from services.OpenData511 import OpenData511Client
 
-load_dotenv(".env")
-
-_api_key_1 = os.getenv("OPEN_DATA_511_API_KEY_1")
-
-OPEN_DATA_511_API_KEY_0 = os.getenv("OPEN_DATA_511_API_KEY_0") or ""
-OPEN_DATA_511_API_KEY_1 = _api_key_1 if _api_key_1 and "<" not in _api_key_1 else None
-OPEN_DATA_511_AGENCY_ID = os.getenv("OPEN_DATA_511_AGENCY_ID") or ""
-OPEN_DATA_511_STOPCODES = os.getenv("OPEN_DATA_511_STOPCODES") or ""
-
-LED_MATRIX_COLS = int(os.getenv("LED_MATRIX_COLS") or -1)
-LED_MATRIX_ROWS = int(os.getenv("LED_MATRIX_ROWS") or -1)
-LED_MATRIX_CHAIN_LENGTH = int(os.getenv("LED_MATRIX_CHAIN_LENGTH") or -1)
-LED_MATRIX_PARALLEL = int(os.getenv("LED_MATRIX_PARALLEL") or -1)
-LED_MATRIX_GPIO_SLOWDOWN = int(os.getenv("LED_MATRIX_GPIO_SLOWDOWN") or -1)
-LED_MATRIX_HARDWARE_MAPPING = os.getenv("LED_MATRIX_HARDWARE_MAPPING") or ""
-LED_MATRIX_MAX_BRIGHTNESS = int(os.getenv("LED_MATRIX_MAX_BRIGHTNESS") or -1)
-
-REFRESH_API_INTERVAL_SECONDS = int(os.getenv("REFRESH_API_INTERVAL_SECONDS") or -1)
-REFRESH_DISPLAY_INTERVAL_SECONDS = int(os.getenv("REFRESH_DISPLAY_INTERVAL_SECONDS") or -1)
-
-RGBMatrix_, _, graphics = get_rgb_matrix_imports()
+RGBMatrix, _, graphics = get_rgb_matrix_imports()
 
 # define globally so it is available to both threads
 display_info_dict: dict[str, DisplayInfoModel] | None = None
@@ -40,10 +27,7 @@ display_info_lock = threading.Lock()
 
 
 def main():
-    threads = [
-        # threading.Thread(target=display_loop),
-        threading.Thread(target=api_loop)
-    ]
+    threads = [threading.Thread(target=display_loop), threading.Thread(target=api_loop)]
     [thread.start() for thread in threads]
 
     # unreachable as threads are while True loops, but waiting for their completion keeps the program running
@@ -54,105 +38,135 @@ def display_loop():
     global display_info_dict
 
     # use bottom-right corner of display for status LED
-    status_led_xy = (LED_MATRIX_COLS - 1, LED_MATRIX_ROWS - 1)
+    status_led_xy = (env.LED_MATRIX_COLS - 1, env.LED_MATRIX_ROWS - 1)
 
     # setup font
     font = graphics.Font()
     font.LoadFont("fonts/5x7.bdf")
-    font_color = graphics.Color(*Colors.MUNI_ALT_LESS)
-    font_color_1 = graphics.Color(*Colors.MUNI_LESS)
+    font_color = graphics.Color(*Colors.MUNI_LESS)
+    font_color_1 = graphics.Color(*Colors.MUNI_ALT_LESS)
 
     # setup matrix and canvas for drawing to display
     matrix = get_rgb_matrix(
-        cols=LED_MATRIX_COLS,
-        rows=LED_MATRIX_ROWS,
-        chain_length=LED_MATRIX_CHAIN_LENGTH,
-        parallel=LED_MATRIX_PARALLEL,
-        gpio_slowdown=LED_MATRIX_GPIO_SLOWDOWN,
-        hardware_mapping=LED_MATRIX_HARDWARE_MAPPING,
-        matrix_brightness=LED_MATRIX_MAX_BRIGHTNESS,
+        cols=env.LED_MATRIX_COLS,
+        rows=env.LED_MATRIX_ROWS,
+        chain_length=env.LED_MATRIX_CHAIN_LENGTH,
+        parallel=env.LED_MATRIX_PARALLEL,
+        gpio_slowdown=env.LED_MATRIX_GPIO_SLOWDOWN,
+        hardware_mapping=env.LED_MATRIX_HARDWARE_MAPPING,
+        matrix_brightness=env.LED_MATRIX_MAX_BRIGHTNESS,
     )
     canvas = matrix.CreateFrameCanvas()
 
     while True:
-        canvas.Clear()
-        # display_info_dict = {
-        #     "13516": DisplayInfoModel(response_timestamp=datetime.now(), stop_visit_list=[StopVisitModel()]),
-        #     "13517": DisplayInfoModel(
-        #         response_timestamp=datetime.now(),
-        #         stop_visit_list=[
-        #             StopVisitModel(
-        #                 recorded_at=datetime.now(),
-        #             )
-        #         ],
-        #     ),
-        # }
         with display_info_lock:
             if display_info_dict is not None:
-                for stopcode, display_info in display_info_dict.items():
-                    display_info.stop_visit_list[0]
+                now = datetime.now(timezone.utc)
+                display_lines: list[str] = []
 
+                for stopcode, display_info_model in display_info_dict.items():
+                    # stop visits come in sorted by expected arrival time at stop, but this is not a guarantee of the
+                    # API docs so sort anyway to guarantee order
+                    sorted_stop_visit_list = sorted(
+                        display_info_model.stop_visit_list,
+                        key=lambda stop: stop.expected_arrival_time
+                        if stop.expected_arrival_time is not None
+                        else datetime.max,
+                    )
+
+                    # group by line reference, with each list ordered by expected arrival time
+                    line_reference_list_map: dict[str, list[StopVisitModel]] = defaultdict(list)
+                    for stop_visit in sorted_stop_visit_list:
+                        # only display
+                        if (
+                            stop_visit.expected_arrival_time is not None
+                            and stop_visit.expected_arrival_time > now
+                            and len(line_reference_list_map[stop_visit.line_reference]) < 2
+                        ):
+                            line_reference_list_map[stop_visit.line_reference].append(stop_visit)
+
+                    for line_reference, stop_visit_list in line_reference_list_map.items():
+                        display_lines.append(
+                            generate_display_line_row(
+                                line_reference,
+                                env.LINE_DISAMBIGUATION_SYMBOL_DICT.get(stopcode, {}).get(line_reference, ""),
+                                [
+                                    sv.expected_arrival_time
+                                    for sv in stop_visit_list
+                                    if sv.expected_arrival_time is not None
+                                ],
+                                now,
+                            )
+                        )
+
+                graphics_display_line_args = []
+                for idx, display_line in enumerate(display_lines):
+                    graphics_display_line_args.append(
+                        (
+                            get_text_center_x_pos(display_line, 5, env.LED_MATRIX_COLS),
+                            1 + ((font.height) * (idx + 1)),
+                            font_color,
+                            display_line,
+                        )
+                    )
+
+                # LED in bottom right corner of display that acts as a visual indicator for how up-to-date the display
+                # info is. Use oldest response timestamp to keep this simple
+                oldest_response_timestamp = min(
+                    [display_info.response_timestamp for display_info in display_info_dict.values()]
+                )
+                status_led_colors = get_status_led_colors(oldest_response_timestamp, env.REFRESH_API_INTERVAL_SECONDS)
+                # do all drawing as close as possible to canvas clear to prevent flickering
+                canvas.Clear()
+                for idx, args in enumerate(graphics_display_line_args):
+                    graphics.DrawText(
+                        canvas,
+                        font,
+                        *args,
+                    )
                 canvas.SetPixel(
                     *status_led_xy,
-                    *get_status_led_colors(display_info_dict["13516"].response_timestamp, REFRESH_API_INTERVAL_SECONDS),
+                    *status_led_colors,
                 )
             else:
-                text0 = "123"
-                text1 = "123"
-                graphics.DrawText(
-                    canvas, font, get_text_center_x_pos(text0, 5, LED_MATRIX_COLS), font.height, font_color, text0
-                )
-                graphics.DrawText(
-                    canvas, font, get_text_center_x_pos(text1, 5, LED_MATRIX_COLS), font.height * 2, font_color, text1
-                )
+                loading_text = "Loading..."
+                text_x_pos = get_text_center_x_pos(loading_text, 5, env.LED_MATRIX_COLS)
+                text_y_pos = get_text_center_y_pos(font.height, env.LED_MATRIX_ROWS)
+
+                canvas.Clear()
                 graphics.DrawText(
                     canvas,
                     font,
-                    get_text_center_x_pos("48W 12 12", 5, LED_MATRIX_COLS),
-                    font.height * 3,
+                    text_x_pos,
+                    text_y_pos,
                     font_color,
-                    "48OB  9 12",
-                )
-                graphics.DrawText(
-                    canvas,
-                    font,
-                    get_text_center_x_pos("48W  9 12", 5, LED_MATRIX_COLS),
-                    font.height * 4,
-                    font_color_1,
-                    "48IA  9 12",
+                    loading_text,
                 )
 
             canvas = matrix.SwapOnVSync(canvas)  # draw canvas, set returned canvas as new canvas to prevent flickering
 
-        sleep(REFRESH_DISPLAY_INTERVAL_SECONDS)
+        sleep(env.REFRESH_DISPLAY_INTERVAL_SECONDS)
 
 
 def api_loop():
     global display_info_dict
 
-    client_list = [OpenData511Client(OPEN_DATA_511_API_KEY_0)]
-    if OPEN_DATA_511_API_KEY_1:
-        client_list.append(OpenData511Client(OPEN_DATA_511_API_KEY_1))
-
-    open_data_stopcode_list = [stopcode for stopcode in OPEN_DATA_511_STOPCODES.split(",") if stopcode]
-
-    if len(open_data_stopcode_list) == 0:
-        open_data_stop_code_env_var_name = f"{OPEN_DATA_511_STOPCODES=}".split("=")[0]
-        raise ValueError(
-            f"Environment variable '{open_data_stop_code_env_var_name}' must be set in .env file at project root"
-        )
+    client_list = [OpenData511Client(env.OPEN_DATA_511_API_KEY_0)]
+    if env.OPEN_DATA_511_API_KEY_1:
+        client_list.append(OpenData511Client(env.OPEN_DATA_511_API_KEY_1))
 
     client_idx = 0
 
     while True:
-        # utilizing a dict to isolate each request such that
+        # utilizing a dict to isolate each stopcode so that request failures only impact the given requested stop
+        # and not all data for all stops
         display_info_dict_staged: dict[str, DisplayInfoModel] = {}
 
-        for stopcode in open_data_stopcode_list:
+        for stopcode in env.OPEN_DATA_511_STOPCODE_LIST:
             try:
                 display_info_dict_staged[stopcode] = (
                     client_list[client_idx]
-                    .get_transit_stop_monitoring(OPEN_DATA_511_AGENCY_ID, stopcode)
+                    .get_transit_stop_monitoring(env.OPEN_DATA_511_AGENCY_ID, stopcode)
                     .convert_to_display_info()
                 )
             except HTTPStatusError as err:
@@ -164,6 +178,7 @@ def api_loop():
                 )
 
         with display_info_lock:
+            # NOTE:
             display_info_dict = display_info_dict or {} | display_info_dict_staged
 
         # round-robin api requests across clients to spread api usage across api keys
@@ -173,7 +188,7 @@ def api_loop():
         if client_idx >= len(client_list):
             client_idx = 0
 
-        sleep(REFRESH_API_INTERVAL_SECONDS)
+        sleep(env.REFRESH_API_INTERVAL_SECONDS)
 
 
 if __name__ == "__main__":
