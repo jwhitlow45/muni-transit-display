048d9819d77a336398e37d6432371f0a46ae23f2
jwhitlow45 <jwhitlow45@gmail.com>
2025-09-21 16:20:43 -0700

    chore:comment out unused members in StopVisitModel for performance

--- models/DisplayInfo.py
+++ models/DisplayInfo.py
@@ -3,20 +3,18 @@ from typing import List
 
 from pydantic import BaseModel
 
-import models.OpenData511 as OpenData511Models
-
 
 class StopVisitModel(BaseModel):
-    recorded_at: datetime
+    # recorded_at: datetime
     line_reference: str
-    line_name: str
-    monitored: bool
-    in_congestion: bool | None
-    occupancy: OpenData511Models.OccupancyEnum | None
-    vehicle_at_stop: bool | None
-    aimed_arrival_time: datetime | None
+    # line_name: str
+    # monitored: bool
+    # in_congestion: bool | None
+    # occupancy: OpenData511Models.OccupancyEnum | None
+    # vehicle_at_stop: bool | None
+    # aimed_arrival_time: datetime | None
     expected_arrival_time: datetime | None
-    aimed_departure_time: datetime | None
+    # aimed_departure_time: datetime | None
 
 
 class DisplayInfoModel(BaseModel):
--- models/OpenData511.py
+++ models/OpenData511.py
@@ -90,24 +90,24 @@ class TransitStopMonitoringResponse(BaseModel):
         response_timestamp = stop_monitoring.ResponseTimestamp
         stop_visit_list = [
             DisplayInfoModels.StopVisitModel(
-                recorded_at=stop_visit.RecordedAtTime,
+                # recorded_at=stop_visit.RecordedAtTime,
                 line_reference=stop_visit.MonitoredVehicleJourney.LineRef,
-                line_name=stop_visit.MonitoredVehicleJourney.PublishedLineName,
-                monitored=stop_visit.MonitoredVehicleJourney.Monitored,
-                in_congestion=stop_visit.MonitoredVehicleJourney.InCongestion,
-                occupancy=stop_visit.MonitoredVehicleJourney.Occupancy,
-                vehicle_at_stop=stop_visit.MonitoredVehicleJourney.MonitoredCall.VehicleAtStop
-                if stop_visit.MonitoredVehicleJourney.MonitoredCall
-                else None,
-                aimed_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
-                if stop_visit.MonitoredVehicleJourney.MonitoredCall
-                else None,
+                # line_name=stop_visit.MonitoredVehicleJourney.PublishedLineName,
+                # monitored=stop_visit.MonitoredVehicleJourney.Monitored,
+                # in_congestion=stop_visit.MonitoredVehicleJourney.InCongestion,
+                # occupancy=stop_visit.MonitoredVehicleJourney.Occupancy,
+                # vehicle_at_stop=stop_visit.MonitoredVehicleJourney.MonitoredCall.VehicleAtStop
+                # if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                # else None,
+                # aimed_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
+                # if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                # else None,
                 expected_arrival_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime
                 if stop_visit.MonitoredVehicleJourney.MonitoredCall
                 else None,
-                aimed_departure_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime
-                if stop_visit.MonitoredVehicleJourney.MonitoredCall
-                else None,
+                # aimed_departure_time=stop_visit.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime
+                # if stop_visit.MonitoredVehicleJourney.MonitoredCall
+                # else None,
             )
             for stop_visit in stop_monitoring.MonitoredStopVisit
             if stop_visit.MonitoredVehicleJourney  # optional from API so ensure existence
